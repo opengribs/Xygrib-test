a74eaaeb54397d157d69bad8154e73719e7b43ee
didier <didier@users.sourceforge.net>
2018-12-01 15:23:35 +0100

    Import degrib 2.18

--- degrib/src/degrib/userparse.h
+++ degrib/src/degrib/userparse.h
@@ -1,5 +1,5 @@
-#define PROGRAM_VERSION "2.17"
-#define PROGRAM_DATE "05/21/2018"
+#define PROGRAM_VERSION "2.18"
+#define PROGRAM_DATE "06/12/2018"
 /*****************************************************************************
  * userparse.h
  *
